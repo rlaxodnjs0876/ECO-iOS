0f5793fbdd299bdbfe9c76b057441160baf814d3
Kim <xodnjs0876@gmail.com>
2023-04-19 20:16:32 +0900

    ⚙️ :: 파일 구조 변경

---
+++ Eco/Eco/EcoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoApp.swift
+//  Eco
+//
+//  Created by 김태원 on 2023/04/18.
+//
+
+import SwiftUI
+
+@main
+struct EcoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
