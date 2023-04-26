93ed3a865c49c2acec5913c738708d23f2942e05
Kim <xodnjs0876@gmail.com>
2023-04-26 12:27:42 +0900

    ⚡️:: Font

---
+++ Eco/Eco/Sources/ResourceCode/font.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+extension UIFont {
+    enum Family: String {
+        case medium = "Medium"
+        case bold = "Bold"
+    }
+
+    static func notoSansFont(ofSize: CGFloat, family: Family) -> UIFont {
+        return UIFont(name: "NotoSansKR-\(family.rawValue)", size: ofSize)!
+    }
+}
